969f5c033177e895c8dedbbab66a8362be959b7e
unknown <st10301625@vcconnect.edu.za>
2023-04-05 09:21:06 +0000

    First Commit

---
+++ src/main/java/com/mycompany/craftpricing/java/CraftPricingJava.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.craftpricing.java;
+import java.util.Scanner ; 
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class CraftPricingJava {
+
+    public static void main(String[] args) {
+        String Details; 
+        double materials ; 
+        double hoursWorked ; 
+        Scanner input = new Scanner(System.in) ; 
+        
+        
+        
+        
+    }
+}
